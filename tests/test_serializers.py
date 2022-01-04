0930485556b67fbf13fc16eb12596946565d6fe8
peder2911 <pglandsverk@gmail.com>
2022-01-04 15:32:53 +0100

    JSON serializer

--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -3,7 +3,7 @@ import string
 import numpy as np
 import pandas as pd
 from pandas.testing import assert_frame_equal
-from views_storage.serializers import csv, parquet, pickle, serializer
+from views_storage.serializers import csv, parquet, pickle, serializer, json
 
 class TestSerializers(unittest.TestCase):
     def assert_serializer_identity(self, dataframe, ser: serializer.Serializer):
@@ -18,3 +18,12 @@ class TestSerializers(unittest.TestCase):
         df = pd.DataFrame(np.random.rand(10,10), columns = list(string.ascii_lowercase[:10]))
         for ser in csv.Csv, parquet.Parquet, pickle.Pickle:
             self.assert_serializer_identity(df, ser())
+
+        ser = json.Json()
+        d = {
+                "a":1,
+                "2":[1,2,3],
+                "3": None,
+                "z": {"foo": 5.5}
+                }
+        self.assertEqual(d, ser.deserialize(ser.serialize(d)))
